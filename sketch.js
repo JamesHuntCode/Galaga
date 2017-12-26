528eeaba91fcacef070420d79b8f91926558693f
James Hunt <jameshuntwork1997@yahoo.com>
2017-12-26 01:08:14 +0000

    Stationary spaceship

--- sketch.js
+++ sketch.js
@@ -1,7 +1,21 @@
+var spaceship;
+
 function setup() {
   createCanvas(600, 600);
+  spaceship = new spaceship((width / 2), (height - height / 6));
 }
 
 function draw() {
   background(51);
+  spaceship.show();
+}
+
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+    // Move left
+  } else if (keyCode === RIGHT_ARROW) {
+    // Move right
+  } else if (key === ' ') {
+    // Shoot
+  }
 }
--- spaceship.js
+++ spaceship.js
@@ -1,13 +1,9 @@
 function spaceship(x, y) {
-  this.x = x;
-  this.y = y;
-  this.size = 20;
-
-  this.move = function() {
-
-  }
+  this.posX = x;
+  this.posY = y;
+  this.size = 40;
 
   this.show = function() {
-
+    triangle(this.posX, this.posY, this.posX - this.size / 2, this.posY + this.size, this.posX + this.size / 2, this.posY + this.size);
   }
 }
